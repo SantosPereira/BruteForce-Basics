e2d75f053689f5ae8acfb8453e720cf05d743064
Pedro <35810476+SantosPereira@users.noreply.github.com>
2021-02-01 09:07:03 -0300

    Update and rename birl3.py to Core/birl3.py

--- Core/birl3.py
+++ Core/birl3.py
@@ -1,15 +1,24 @@
 print('BruteForceAlg v3.0')
 
+
+"""	Senha inserida pelo usuário e que o algoritmo vai tentar achar através da força bruta, combinando uma cadeia de caracteres	"""
 senha = input('Insira sua senha: ')
 
+
+"""	Caracteres para testagem de combinação	"""
 p = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9']
+
+
+"""	Variáveis de contagem	"""
 x = -1
 y = -1
 r = '0'
 
+
+"""	Loop de testagem	"""
 while r != senha:
 	x= -1
-	for i in range(0,35):
+	for i in range(0,35): #35 é o número de caracteres para a combinação (alfabeto + 0_9 - 1)
 		if r == senha:
 			break
 		x+=1
